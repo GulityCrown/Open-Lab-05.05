00a22381a84fc2ecd51346a07bbd6ba05aaaf99e
GulityCrown <109987666+GulityCrown@users.noreply.github.com>
2022-11-08 08:00:11 +0100

    Update StringTools.cs

--- Open-Lab-05.05/StringTools.cs
+++ Open-Lab-05.05/StringTools.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using System;
+using System.Linq;
+using System.ComponentModel;
+using System.Globalization;
 
 namespace Open_Lab_05._05
 {
@@ -6,7 +9,46 @@ namespace Open_Lab_05._05
     {
         public string AlternatingCaps(string original)
         {
-            throw new NotImplementedException();
+            original = original.ToLower();
+            string modified = "";
+            int i = 0;
+            int pM = 0;
+
+            while (i < original.Length)
+            {
+                if (original[i] == ' ' && i == 0)
+                {
+                    modified += original[0];
+                }
+                else if (i == 0 && original[i] != ' ')
+                    modified += char.ToUpper(original[i]);
+                else if (modified[i - 1] == char.ToUpper(original[i - 1]) && original[i] != ' ' && original[i - 1] != ' ')
+                {
+                    modified += char.ToLower(original[i]);
+                }
+                else if (modified[i - 1] == char.ToLower(original[i - 1]) && original[i - 1] != ' ')
+                {
+                    modified += char.ToUpper(original[i]);
+                }
+                else if (original[i] == ' ')
+                {
+                    modified += original[i];
+                    pM++;
+
+                }
+                else if (original[i - 1] == ' ' && original[i] != ' ' && original[i - pM - 1] == modified[i - pM - 1] && i != 0)
+                {
+                    modified += char.ToUpper(original[i]);
+                    pM = 0;
+                }
+                else if (original[i - 1] == ' ' && original[i] != ' ' && char.ToUpper(original[i - pM - 1]) == modified[i - pM - 1] && i != 0)
+                {
+                    modified += original[i];
+                    pM = 0;
+                }
+                i++;
+            }
+            return modified;
         }
     }
 }
